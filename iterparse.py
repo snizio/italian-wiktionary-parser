f4c0e37bede5b63338b6011d578c36852af8df67
snizio <cristianociaccio@gmail.com>
2023-07-29 18:12:35 +0200

    added frequent english words in italian that do not show italian as a first glossa language; also documented code

--- iterparse.py
+++ iterparse.py
@@ -8,19 +8,21 @@ import sys
 
 
 def prepend_ns(s):
-    """This prepend the namespace to each tag"""
+    """This prepend the namespace to each tag. It's useful to retrieve tags with the xml module."""
     return NAMESPACE + s 
 
 def remove_list_tokens(line):
+    """Recursively removes special tokens (*, #, :) at the beginning of a line in a list."""
     if line[0] in ["*", "#", ":"]:
         return remove_list_tokens(line[1:])
     else:
         return line
 
 def lang_check(line, lemma):
-    line = line.replace(" ", "") # eliminiamo gli spazi per conformità
-    match =  re.search(lang_pattern, line) # lingua
-    if match != None:
+    """Check for the italian language tag, usually something like =={{-it-}}==."""
+    line = line.replace(" ", "") # removing white spaces for conformity
+    match =  re.search(lang_pattern, line) 
+    if match != None: # if we found a lang tag
         lang = match.group(1)
         if lang == None:
             lang = match.group(2)
@@ -33,11 +35,13 @@ def lang_check(line, lemma):
         return 0, False
     
 def pron_check(line):
+    """Checks for the pronouce tag {{pron}}"""
     match = re.search(pron_pattern, line)
     if match != None:
         return True
 
 def get_ipa(line, lemma):
+    """Extracts IPA from a line"""
     match = re.search(ipa_pattern, line) # ipa
     if match != None:
         ipa = match.group(1)
@@ -47,31 +51,27 @@ def get_ipa(line, lemma):
         return False
     
 def sill_check(line):
-    """To check if the sill token is present in the line"""
+    """Checks for the {{sill}} tag"""
     match = re.search(sill_pattern, line) # sill
     if match != None:
         return True
     
 def get_sill(line, lemma):
-    if line[0] == ";":
-        parsed_dict[lemma]["meta"]["sill"] = line[1:].replace(" ", "").split("|")
-        return True
-    else:
-        return False
-
-def pos_check(line):
-    match = re.search(pos_pattern, line) # pos
-    if match != None:
+    """Extracts the syllables from a line"""
+    if line[0] == ";": 
+        parsed_dict[lemma]["meta"]["sill"] = line[1:].replace(" ", "").split("|") # it splits the sill string into ["ca", "sa"] for example
         return True
     else:
         return False
 
 def unk_pos(lemma):
-    pos = "unk" # opzione di default (poichè il formato non è consistente)
+    """Adds the "unk" (unknown) PoS tag to a lemma"""
+    pos = "unk" # default option due to inconsistencies in the italian wiktionary tag system
     parsed_dict[lemma]["meanings"][pos] = {"morpho":"", "glossa":""}
     return pos
 
 def check_pos(lemma, line, i_pos, current_pos):
+    """Checks for the PoS pattern and retrieves the available PoS"""
     match = re.search(pos_pattern, line) # pos
     if match != None:
         pos = match.group(1) + f"_{i_pos}"
@@ -79,12 +79,13 @@ def check_pos(lemma, line, i_pos, current_pos):
             current_pos = pos
             parsed_dict[lemma]["meanings"][pos] = {"morpho":"", "glossa":""}
             if "unk" in parsed_dict[lemma]["meanings"]:
-                del parsed_dict[lemma]["meanings"]["unk"] # se dopo aver messo unk si trova una pos si elimina unk
+                del parsed_dict[lemma]["meanings"]["unk"] # if we find a PoS we delete the "unk" one
         return pos
     else:
         return current_pos
     
 def morpho_check(line, lemma, pos):
+    """Checks and extracts morphological metadata (i.e. "f sing" from a typical morpho line: {{Pn|w}} ''f sing'' )"""
     match = re.search(morpho_pattern, line) # informazioni morfologiche
     if match != None:
         morpho = match.group(1).lstrip() # talvolta c'è uno spazio all'inizio
@@ -92,16 +93,19 @@ def morpho_check(line, lemma, pos):
         return True
 
 def etim_check(line):
+    """Checks for the {{etim}} tag"""
     match = re.search(etim_pattern, line)
     if match != None:
         return True
 
 def noetim_check(line):
+    """Checks fot the {{Noetim|it}} tag. Usually used when the etim is missing"""
     match = re.search(noetim_pattern, line)
     if match != None:
         return True
 
 def get_etim(line, lemma):
+    """Extracts and parses the etim"""
     line = remove_list_tokens(line)
     cleaned_line = re.sub("{{Pn}}|{{pn}}", lemma, line)
     cleaned_line = re.sub(ref_pattern, "", cleaned_line)
@@ -121,6 +125,7 @@ def get_etim(line, lemma):
         parsed_dict[lemma]["meta"]["etim"] += "\n"+cleaned_line
     
 def glossa_check(line, lemma, pos):
+    """Extracts and parses the glossa"""
     line = remove_list_tokens(line)
     cleaned_line = re.sub("{{Pn}}|{{pn}}", lemma, line)
     cleaned_line = re.sub(ref_pattern, "", cleaned_line)
@@ -140,19 +145,20 @@ def glossa_check(line, lemma, pos):
 
 
 def main(xml_dump_path):
+    """main function"""
 
-    context = iterparse(xml_dump_path, events=("start", "end"))
+    context = iterparse(xml_dump_path, events=("start", "end")) # iterparse iterator object
 
     for event, elem in tqdm(context, desc="Parsing XML", unit=" elements"):
         if event == "end":
-            if prepend_ns("page") == elem.tag:
-                lemma = elem.find(prepend_ns("title"))
+            if prepend_ns("page") == elem.tag: # if we find a page (usually there is a page for each lemma)
+                lemma = elem.find(prepend_ns("title")) # the title tag contains the lemma string
                 if lemma != None:
                     lemma = str(lemma.text)
                     if ":" in lemma or lemma in ["Pagina principale", "Pagina principale/Categorie"]:
                         continue
                     revision = elem.find(prepend_ns("revision"))
-                    glossa = revision.find(prepend_ns("text")).text
+                    glossa = revision.find(prepend_ns("text")).text # this contains all the metadata for a specific lemma
                     current_pos = ""
                     lang_found = False
                     sill_flag = False
@@ -168,15 +174,12 @@ def main(xml_dump_path):
                             line = lines[i]
 
                             if line == "":
-                                # etim_flag = False
-                                # pron_flag = False
-                                # sill_flag = False
                                 continue
 
                             if pron_flag:
                                 pron_flag = get_ipa(line, lemma)
                                 if pron_flag:
-                                    continue # di ipa possono essercene più di una
+                                    continue # there can be more than one IPA
 
                             if sill_flag:
                                 found = get_sill(line, lemma)
@@ -200,15 +203,18 @@ def main(xml_dump_path):
 
                             if line.find("{{Vedi|") != -1:
                                 continue
-                            if line[0] == "[": #immagini
+                            if line[0] == "[": # images
                                 continue
 
                             if line[0] == "=":
                                 lang, lang_found = lang_check(line, lemma)
-                                if lang == 0: # se la line inizia con "=" ma non ha informazioni sulla lingua 
+                                if lang == 0: # if the line starts with "=" but do not contains lang information (rare)
                                     continue
                                 if lang != "it":
-                                    break
+                                    if parsed_dict.get(lemma, None) != None: # if lang is different form "it" and we already have the lemma in the dict
+                                        break # it means we already got an italian tag and we are moving into a different language, so we break
+                                    else:
+                                        continue # else we keep on iterating hoping to find an italian tag
                                 if lang_found:
                                     continue
                             
@@ -232,7 +238,7 @@ def main(xml_dump_path):
                             
                             sill_flag = sill_check(line)
                             if sill_flag:
-                                continue # se trovo il token della sill allora passo alla iter successiva
+                                continue
                             
                             if morpho_check(line, lemma, current_pos):
                                 continue
@@ -242,26 +248,26 @@ def main(xml_dump_path):
                                 continue
 
                             if line[0] == "#": # glossa
-                                if line[-1] == ":": # introduce elenco
+                                if line[-1] == ":": # it introduces a list (usually...)
                                     elenco_flag = True
                                 try:
-                                    if line[1] == "*" and not elenco_flag: # esempio
+                                    if line[1] == "*" and not elenco_flag: # examples
                                         continue
                                 except IndexError:
-                                    continue # linea[1:] è vuota
+                                    continue # if line[1:] is empty
                                 glossa_check(line, lemma, current_pos)
                             else:
                                 elenco_flag = False
                                                 
                     except Exception as e:
-                        print("ERRORE al lemma", lemma)
-                        raise
+                        print("ERROR at lemma", lemma)
+                        raise e
 
-    del context
+    del context # deleting the context to free memory
 
 if __name__ == "__main__":
     
-    # load and convert to dictionary with the most frequent iso 639-1 language codes, those are used by wikitionary usually {{la}} ---> Latino
+    # load and convert to dictionary with the most frequent iso 639-1 language codes, those are used by the wiktionary usually like {{la}} ---> Latino
     df = pd.read_csv("lang_list.tsv", sep="\t")
     lang_dict = {k:v for k, v in zip(df["Language Code"], df["Language Name (Italian)"])}
 
@@ -276,6 +282,7 @@ if __name__ == "__main__":
     NAMESPACE = root.tag.split("}")[0] + "}"
     del context
 
+    # compiling regex
     lang_pattern = re.compile("=={{-?(.+?)-?}}==")
     vedi_pattern = re.compile("{{[Vv]d\|(.*?)}}")
     pos_pattern = re.compile("{{-(.*?)-\|(?:\|?\w*)*}}")
